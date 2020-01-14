8444f870c1951b005f920eccccb8153eaf12570b
ZHJD <2262040671@qq.com>
2020-01-14 15:37:40 +0800

    添加adjust_up()

--- MinHeap/MinHeap.cpp
+++ MinHeap/MinHeap.cpp
@@ -6,10 +6,12 @@ using namespace std;
 int main()
 {
     MinHeap minheap;
-    vector<int> heap{3,1,2,7,5,0,100,-1,16,17,19,-6};
+    vector<int> heap{4,5,8,2};
     minheap.heapify(heap);
     cout << minheap.pop() << endl;
-    minheap.push(-200);
+    
+    minheap.push(3);
+    cout << minheap.pop() << endl;
     cout << minheap.pop() << endl;
     return 0;
 }
--- MinHeap/MinHeap.h
+++ MinHeap/MinHeap.h
@@ -16,8 +16,7 @@ public:
     void push(int elem)
     {
         m_heap.push_back(elem);
-        std::swap(m_heap[0], m_heap[m_heap.size() - 1]);
-        adjust_down(0, m_heap.size());
+        adjust_up();
     }
     // 弹出一个元素
     int pop()
@@ -28,30 +27,50 @@ public:
         adjust_down(0, m_heap.size());
         return elem;
     }
+
+    int size()
+    {
+        return m_heap.size();
+    }
 private:
     std::vector<int> m_heap;
     // 向下调整[i,j)堆
     void adjust_down(int i, int j)
     {
-        int temp = m_heap[i];
-        while(i < j)
+       int temp = m_heap[i];
+       int k = i * 2 + 1; //左孩子
+       while(k < j)
+       {
+           // 选择最小元素进行交换
+           if(k < (size() - 1) && m_heap[k + 1] < m_heap[k]) 
+           {
+               k++;
+           }
+           if(m_heap[k] < temp) // 比堆顶元素小
+           {
+               m_heap[i] = m_heap[k];
+               i = k;
+               k = 2 * k + 1;
+           }
+           else
+           {
+               break;
+           }
+       }
+       m_heap[i] = temp;
+    }
+
+    void adjust_up()
+    {
+        int j = m_heap.size() - 1;
+        int temp = m_heap[j];
+        int i = (j + 1) / 2 - 1;
+        while(i >= 0 && temp < m_heap[i])
         {
-            int k = i * 2 + 1;
-            // 选择两个子结点中的最小值
-            if(m_heap[k + 1] < m_heap[k])
-            {
-                k++;
-            }
-            if(m_heap[k] < temp)
-            {
-                m_heap[i] = m_heap[k];
-            }
-            else
-            {
-                m_heap[i] = temp;
-                break;
-            }
-            i = k;
+            m_heap[j] = m_heap[i];
+            j = i;
+            i = (i + 1) / 2 - 1;
         }
+        m_heap[j] = temp;
     }
 };
